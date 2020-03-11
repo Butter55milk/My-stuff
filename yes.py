a85c7709864cc39cdbfb20e053696ca2e5884cc0
Butter55milk <61983798+Butter55milk@users.noreply.github.com>
2020-03-11 08:50:02 +0200

    Uploaded file

---
+++ yes.py
@@ -0,0 +1,26 @@
+import random as no
+for i in range(10):
+	No1 = no.randrange(9)
+	No2 = no.choice(['Valintino', 'Chris', 'buttermilk'])
+	No3 = no.randrange(9)
+	No4 = no.randrange(9)
+	No5 = no.randrange(9)
+	No6 = no.randrange(9)
+	No7 = no.randrange(9)
+for i in range(10):
+	No8 = no.randrange(9)
+	No9 = no.choice(['yes66', 'Christopher', 'Valintino4'])
+	No10 = no.randrange(9)
+	No11 = no.randrange(9)
+	No12 = no.randrange(9)
+for i in range(9):
+	No13 = no.choice(['cviviers', 'Chriso', 'butter55milk'])
+	No14 = no.randrange(9)
+	No15 = no.randrange(9)
+	No16 = no.randrange(9)
+	No17 = no.randrange(9)
+PS1 = No1, No2, No3, No4, No5, No6,  No7
+PS2 = No8, No9, No10, No11, No12
+PS3 = No13, No14, No15, No16, No17
+PassW = no.choice([PS1, PS2, PS3])
+print(PassW)
